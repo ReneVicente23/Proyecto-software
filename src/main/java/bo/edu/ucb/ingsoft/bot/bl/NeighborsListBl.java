3a2c1e0b902df143e3035298b1ac73776dafa131
JosePozoSilva <jhosede23@gmail.com>
2022-04-21 19:25:35 -0400

    modificando array de ejemplo

--- src/main/java/bo/edu/ucb/ingsoft/bot/bl/NeighborsListBl.java
+++ src/main/java/bo/edu/ucb/ingsoft/bot/bl/NeighborsListBl.java
@@ -16,7 +16,11 @@ public class NeighborsListBl {
 
         List<NeighborListDto>result = new ArrayList<>();
        result.add(new NeighborListDto("Jose Armando", "Pozo Silva","8440004","24"
-       ,"65578478","jhosede23@gmail.com","Bajo LLojeta","s"));
+       ,"65578478","jhosede23@gmail.com","Bajo LLojeta Sur","foto1"));
+
+        result.add(new NeighborListDto("Prueba", "Demo","1212","01"
+                ,"67777","prueba1@prueba.prueba","pruba 2","foto"));
+
         return result;
     }
 }
